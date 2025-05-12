5b4b655a1999e999a97b895a3bd1a51d313f9bd0
Mohinish-k053 <mohinish.k053@gmail.com>
2025-05-12 12:38:28 +0530

    ecs_log_delete

--- ecs_log_del.sh
+++ ecs_log_del.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+# Disable Git Bash automatic path conversion
+export MSYS_NO_PATHCONV=1
+
 echo "Searching for ECS-related CloudWatch log groups..."
 
-# Get all log groups, one per line, and filter only ECS ones
 aws logs describe-log-groups --query 'logGroups[].logGroupName' --output text | tr '\t' '\n' | grep -E '^/ecs/|^/aws/ecs/' | while read -r LOG_GROUP; do
-  LOG_GROUP_TRIMMED=$(echo "$LOG_GROUP" | xargs)
-  echo "Deleting log group: $LOG_GROUP_TRIMMED"
-  aws logs delete-log-group --log-group-name "$LOG_GROUP_TRIMMED"
+  echo "Deleting log group: $LOG_GROUP"
+  aws logs delete-log-group --log-group-name "$LOG_GROUP"
 done
+
 echo "All ECS-related log groups deleted."
